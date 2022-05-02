cbb877cab1139f92e23403ff35b1092c31bdeaa3
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-05-02 15:46:33 -0300

    Adicionando método Get pelo Id da postagem

--- src/main/java/com/generation/blogPessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogPessoal/controller/PostagemController.java
@@ -3,9 +3,11 @@ package com.generation.blogPessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,12 @@ public class PostagemController {
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAllPostagem(){
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+		return repository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 }
