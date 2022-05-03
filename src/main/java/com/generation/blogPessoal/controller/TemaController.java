cada9fb01fd210eaa02722b6aa1d187ff1128cd7
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-05-02 22:43:25 -0300

    Adicionando método post() ao TemaController

--- src/main/java/com/generation/blogPessoal/controller/TemaController.java
+++ src/main/java/com/generation/blogPessoal/controller/TemaController.java
@@ -2,11 +2,16 @@ package com.generation.blogPessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogPessoal.model.Tema;
@@ -34,5 +39,10 @@ public class TemaController {
 	@GetMapping("/nome")
 	public ResponseEntity<List<Tema>> getByName(@PathVariable("nome") String nome){
 		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(nome));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Tema> post(@Valid @RequestBody Tema tema){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
 	}
 }
