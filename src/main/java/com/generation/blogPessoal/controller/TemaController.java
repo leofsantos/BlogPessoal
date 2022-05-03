f8e6605bcb141c203eab0dca75470a2a4de1cb33
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-05-02 22:37:29 -0300

    Adicionando método getById() ao TemaController

--- src/main/java/com/generation/blogPessoal/controller/TemaController.java
+++ src/main/java/com/generation/blogPessoal/controller/TemaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogPessoal.model.Tema;
@@ -21,5 +22,12 @@ public class TemaController {
 	@GetMapping
 	public ResponseEntity<List<Tema>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Tema> getById(@PathVariable("id") Long id){
+		return repository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
