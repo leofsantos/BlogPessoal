686836dc944baff917712641ce7b1204a340d218
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-05-02 22:32:03 -0300

    Adicionando método getAll() ao TemaController

---
+++ src/main/java/com/generation/blogPessoal/controller/TemaController.java
@@ -0,0 +1,25 @@
+package com.generation.blogPessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogPessoal.model.Tema;
+import com.generation.blogPessoal.repository.TemaRepository;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class TemaController {
+
+	@Autowired
+	private TemaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Tema>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+}
