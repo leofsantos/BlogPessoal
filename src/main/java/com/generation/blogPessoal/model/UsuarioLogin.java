a47884fe8be681107cc35c20efd3ed30df420c0e
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-05-16 14:26:09 -0300

    Inserção dos construtores

--- src/main/java/com/generation/blogPessoal/model/UsuarioLogin.java
+++ src/main/java/com/generation/blogPessoal/model/UsuarioLogin.java
@@ -12,7 +12,24 @@ public class UsuarioLogin {
 	
 	private String foto;
 	
+	private String tipo;
+	
 	private String token;
+
+	public UsuarioLogin(long id, String nome, String usuario, String senha, String foto, String tipo, String token) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+		this.tipo = tipo;
+		this.token = token;
+	}
+
+	public UsuarioLogin() {
+		super();
+	}
 
 	public long getId() {
 		return id;
@@ -52,6 +69,14 @@ public class UsuarioLogin {
 
 	public void setFoto(String foto) {
 		this.foto = foto;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
 	}
 
 	public String getToken() {
